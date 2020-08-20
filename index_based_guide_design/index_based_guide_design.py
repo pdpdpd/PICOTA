059c054bda5ef3a6b91e261d6aeca9d7642d9cf5
Yidan Pan <yidan.pan@rice.edu>
2020-08-19 20:55:46 -0500

    Update auto primer design script

--- index_based_guide_design/index_based_guide_design.py
+++ index_based_guide_design/index_based_guide_design.py
@@ -1,9 +1,27 @@
+'''
+Todo:
+1 get primer3 settings for NGS primer design
+2 run primer3 (3 pairs per position)
+3 run in-silico PCR on command-line
+4 Check if primers are good.
+'''
 
-# ---------------------------------import---------------------------------------
 
+
+# - environment requirement
+# conda install -c bioconda primer3-py
+# conda install -c anaconda cython
+# -------------------------------import---------------------------------------
+
+import urllib.request
+import primer3
+import math
+import time
+import random
+import socket
+import re
 import sys, csv, operator
-import argparse
-
+import ssl
 # ---------------------------------import---------------------------------------
 
 
@@ -20,26 +38,116 @@ def my_sq_range(start, end, step):
 
 
 # find the flanking sequences for primer design
-def flanking_seq(chr, start_pos):
+def flanking_seq(ref, chr, ot_start, amp_max):
     try:
-        start_ = int(snp_) - 30 + 250
-        end_ = int(snp_) + 30 + 250  # now the position is not at the begining of where the SNP starts
-    except:
-        print
-        "Wrong pos"
-    else:
-        url_ = "http://genome.ucsc.edu/cgi-bin/das/hg19/dna?segment=" + chr_ + "%3A" + str(start_) + "," + str(end_)
+        flank_start = max(1, int(ot_start) - amp_max)  # set to 1 if it is too close to the chr starting position
+        flank_end = int(ot_start) + 30 + amp_max  # Assuming the length of gRNA is at most 30
+        dna_length = flank_end - flank_start + 1
+    except ValueError:
+        print("Wrong position information")
 
-        # print url_
-        req_ = urllib2.Request(url_)
-        resp_ = urllib2.urlopen(req_)
-        respHtml_ = resp_.read()
-        DNA_seq = respHtml_[respHtml_.find('</DNA>') - 63:respHtml_.find('</DNA>')]
-        DNA_ = DNA_seq.replace('\n', '')
-        # print "DNA_"+DNA_
-    return DNA_
+    else:
+        url = "http://genome.ucsc.edu/cgi-bin/das/"+ref+"/dna?segment=" \
+              + chr + "%3A" + str(flank_start) + "," + str(flank_end)
+
+        # print(url)
+        # req = urllib.request(url)
+        req = urllib.request.Request(url)
+        try:
+            with urllib.request.urlopen(req) as response:
+                resp_html = response.read()
+                resp_html_str = str(resp_html)
+                flank_seq = resp_html_str[resp_html_str.find('</DNA>') - dna_length-28:resp_html_str.find('</DNA>')]
+                seq_array = flank_seq.split("\\n")
+        except urllib.error.URLError as e:
+            print(e.reason)
+    str_empty = ''
+    return str_empty.join(seq_array)
+
+
+# Generate required setting file for primer3
+def primer_design_settings(ref_seq, amplicon_min, amplicon_max):
+    print("rua")
+    # os.system(qry + ' >> ' + output_dir)
 
 # ---------------------------------functions------------------------------------
+
+
+# testing main
+# original csv: chr19,41352729,CCATTAGCACGCGGGTGACCTCCTTGGCGTAGTAGTCGGCCTCAGGCTCG
+# output = flanking_seq("hg38", "chr19", 41352729, 320)
+# print(output)
+output = 'cctctaggggactgcccccacgaccccgcatgtttctgtcgcactctagaagcggtccacttcgctatctcctcctctcca' \
+         'agaccagacacctgggtggtagggggctcagtgccatcctctttcggacacccccctcccaccatcacacgttccctttgc' \
+         'cccggggtgtcctcttcctccagccagtttcttctgccagtcacttcctacccgtggccccggcactccggcgccccctgg' \
+         'gggcccccctcccggctcccctgcccctccgagctcaccgttgtgggtttccaccattagcacgcgggtgacctccttggc' \
+         'gtagtagtcggcctcaggctcgggctccggttctgcactctccccggccacccggtcgcgggtgctgttgtacagggcgag' \
+         'cacggcctcgggcagcgggccgggcggcacctccccctggctcggggggctggcgagccgcagcttggacaggatctggcc' \
+         'gcggatggcctcgatgcgcttccgcttcaccagctccatgtcgatagtcttgcaggtggatagtcccgcggccggccggcc' \
+         'aggcgtcagcaccagtagccacagcagcggtagcagcagcggcagcagccgcagcccggagggcggcatgggggaggcggc' \
+         'gccccccggcactgccgagagcg'
+
+test = primer3.bindings.designPrimers(
+    {
+        'SEQUENCE_ID': 'MH1000',
+        'SEQUENCE_TEMPLATE': 'GCTTGCATGCCTGCAGGTCGACTCTAGAGGATCCCCCTACATTTT' \
+                              'AGCATCAGTGAGTACAGCATGCTTACTGGAAGAGAGGGTCATGCA' \
+                              'ACAGATTAGGAGGTAAGTTTGCAAAGGCAGGCTAAGGAGGAGACG' \
+                              'CACTGAATGCCATGGTAAGAACTCTGGACATAAAAATATTGGAAG' \
+                              'TTGTTGAGCAAGTNAAAAAAATGTTTGGAAGTGTTACTTTAGCAA' \
+                              'TGGCAAGAATGATAGTATGGAATAGATTGGCAGAATGAAGGCAAA' \
+                              'ATGATTAGACATATTGCATTAAGGTAAAAAATGATAACTGAAGAA' \
+                              'TTATGTGCCACACTTATTAATAAGAAAGAATATGTGAACCTTGCA' \
+                              'GATGTTTCCCTCTAGTAG',
+        'SEQUENCE_INCLUDED_REGION': [36,342]
+    },
+    {
+        'PRIMER_OPT_SIZE': 20,
+        'PRIMER_PICK_INTERNAL_OLIGO': 1,
+        'PRIMER_INTERNAL_MAX_SELF_END': 8,
+        'PRIMER_MIN_SIZE': 18,
+        'PRIMER_MAX_SIZE': 25,
+        'PRIMER_OPT_TM': 60.0,
+        'PRIMER_MIN_TM': 57.0,
+        'PRIMER_MAX_TM': 63.0,
+        'PRIMER_MIN_GC': 20.0,
+        'PRIMER_MAX_GC': 80.0,
+        'PRIMER_MAX_POLY_X': 100,
+        'PRIMER_INTERNAL_MAX_POLY_X': 100,
+        'PRIMER_SALT_MONOVALENT': 50.0,
+        'PRIMER_DNA_CONC': 50.0,
+        'PRIMER_MAX_NS_ACCEPTED': 0,
+        'PRIMER_MAX_SELF_ANY': 12,
+        'PRIMER_MAX_SELF_END': 8,
+        'PRIMER_PAIR_MAX_COMPL_ANY': 12,
+        'PRIMER_PAIR_MAX_COMPL_END': 8,
+        'PRIMER_PRODUCT_SIZE_RANGE': [[75,100],[100,125],[125,150],
+                                      [150,175],[175,200],[200,225]],
+    })
+print(test['PRIMER_RIGHT_0_SEQUENCE'], test['PRIMER_LEFT_0_SEQUENCE'])
+
+
+# g = ['catcaggagagcgcttgctcacccaccccctctctattgatttgttcact', 'gtgttaatggctcccttttgtgtttggctggtcagtgttttgaattggtc', 'tttaattatatctcatgcaaatgactaaggcatatggagaggttgtggag', 'ccaaaagtttagaagcacctattatgtgccactcctactactcttctctg', 'caacataaatagaggaaactgaggctcggaggagttaagtaaaacactgg', 'tcaagttggcccaggggcccacatctcctgaccccagcataggctcttta', 'tgagtttggatggatgagatctttggatgagatatctcagaacaaggagc', 'cagcctgggcaatccgtttgccaagctagagtcactgagcaagccagaca', 'cctacagatgccacaggtaggaacaactgctgtgtactctagctcagctt', 'gagaatagttggtttaatttatgaatttagatggttgaaaaaccaaaagc', 'catagaaacaatcagcacttgtcaagtgcctactatgtgccaggaacttc', 'tgtttcccccttttcatctgtgaaatggggatgacagtaaaataatgcca', 'acctctcctgattgatgaaggatcaagtgagagccatggaatcacttagc', 'acagaggctgtacttaatatg', '']
+
+# str = ''
+# print(str.join(g))
+
+
+'''
+
+f = 'catcaggagagcgcttgctcacccaccccctctctattgatttgttcact\ngtgttaatggctcccttttgtgtttggct' \
+    'ggtcagtgttttgaattggtc\ntttaattatatctcatgcaaatgactaaggcatatggagaggttgtggag\nccaaaagtt' \
+    'tagaagcacctattatgtgccactcctactactcttctctg\ncaacataaatagaggaaactgaggctcggaggagttaagtaaaacactg' \
+    'g\ntcaagttggcccaggggcccacatctcctgaccccagcataggctcttta\ntgagtttggatggatgagatctttggatgagatatctcagaac' \
+    'aaggagc\ncagcctgggcaatccgtttgccaagctagagtcactgagcaagccagaca\ncctacagatgccacaggtaggaacaactg' \
+    'ctgtgtactctagctcagctt\ngagaatagttggtttaatttatgaatttagatggttgaaaaaccaaaagc\ncatagaaacaatcagca' \
+    'cttgtcaagtgcctactatgtgccaggaacttc\ntgtttcccccttttcatctgtgaaatggggatgacagtaaaataatgcca\nacctctcctgat' \
+    'tgatgaaggatcaagtgagagccatggaatcacttagc\nacagaggctgtacttaatatg\n'
+
+strings = re.findall(r"\S+", f)
+str1 = ''
+
+print(str1.join(strings))
 
 
 # get parsed information
@@ -51,3 +159,5 @@ parser.add_argument('--min', default=280, type=int,
 parser.add_argument('--max', default=320, type=int,
                     help='Maximum amplicon size.')
 args = parser.parse_args()
+
+'''
